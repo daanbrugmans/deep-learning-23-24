0a1ed0e111f02d5487c6e64b2add390bc278a3e0
Maximilian Pohl <maximilian@tweedegolf.com>
2023-10-25 16:10:23 +0200

    Add pruning

--- Assignment 7/dl_assignment_7_common.py
+++ Assignment 7/dl_assignment_7_common.py
@@ -1,8 +1,10 @@
 # This is a file where you should put your own functions
 import math
-from typing import Dict, Any
+from typing import Dict, Any, Iterator
 
+from d2l.torch import try_gpu
 from torch import nn
+from torch.nn import CrossEntropyLoss
 from torchvision.datasets import MNIST, FashionMNIST
 from torch.utils.data import DataLoader
 import torchvision
@@ -13,7 +15,7 @@ from d2l import torch as d2l
 # -----------------------------------------------------------------------------
 # Datasets
 # -----------------------------------------------------------------------------
-def get_dataset(name: str, batch_size=60, used_data=1.0) -> dict[str, DataLoader[Any]]:
+def get_datasets(name: str, batch_size=60, used_data=1.0) -> dict[str, DataLoader[Any]]:
     """
     :param int batch_size: batch size to use for the DataLoaders
     :param str name: name of the dataset to load. Available are 'mnist' and 'fashionmnist'
@@ -60,7 +62,7 @@ def get_dataset(name: str, batch_size=60, used_data=1.0) -> dict[str, DataLoader
 # -----------------------------------------------------------------------------
 
 # TODO: Define network architectures here
-def create_lenet(num_classes=10):
+def _create_lenet(num_classes=10):
     return nn.Sequential(
         # I could not find the activation function used in the paper, but other implementations
         # of LeNet seem to use sigmoid
@@ -70,16 +72,16 @@ def create_lenet(num_classes=10):
     )
 
 
-def create_arch2():
+def _create_arch2():
     raise NotImplementedError()
 
 
 def create_network(arch, **kwargs):
     # TODO: Change this function for the architectures you want to support
     if arch == 'lenet':
-        net = create_lenet(**kwargs)
+        net = _create_lenet(**kwargs)
     elif arch == 'arch2':
-        net = create_arch2(**kwargs)
+        net = _create_arch2()
     else:
         raise ValueError(f"Architecture name {arch} was not recognized")
     return net
@@ -124,9 +126,9 @@ def evaluate(net, evaluation_set, loss_fn, device):
     return loss, accuracy
 
 
-def train(net: nn.Module, data_loaders: Dict[str, DataLoader], optimizer: torch.optim, loss_fn: nn,
+def train(net: nn.Module, data_loaders: Dict[str, DataLoader], optimizer, loss_fn,
           device: str, model_file_name: str, learning_rate: float = 0.0012, epochs: int = 50000,
-          eval_every_n_epochs=100):
+          eval_every_n_epochs=100, graph=True):
     # inspired by assignment 5
     """
     Trains the model net with data from the data_loaders['train'], data_loaders['val'], data_loaders['test'].
@@ -134,12 +136,14 @@ def train(net: nn.Module, data_loaders: Dict[str, DataLoader], optimizer: torch.
     net = net.to(device)
     optimizer = optimizer(net.parameters(), lr=learning_rate)
 
-    training_progression_animator = d2l.Animator(
-        xlabel='epoch',
-        legend=['train loss', 'train accuracy', 'validation loss', 'validation accuracy'],
-        figsize=(10, 5)
-    )
+    if graph:
+        training_progression_animator = d2l.Animator(
+            xlabel='epoch',
+            legend=['train loss', 'train accuracy', 'validation loss', 'validation accuracy'],
+            figsize=(10, 5)
+        )
     training_progression_timer = {'train': d2l.Timer(), 'val': d2l.Timer()}
+    min_val_loss = float("inf")
 
     for epoch in range(epochs):
         # monitor loss, accuracy, number of samples
@@ -172,20 +176,27 @@ def train(net: nn.Module, data_loaders: Dict[str, DataLoader], optimizer: torch.
                 training_progression_timer[phase].stop()
 
         if epoch % eval_every_n_epochs == 0:
+            val_loss = metrics['val'][0] / metrics['val'][2]
+            if val_loss < min_val_loss:
+                path = f"checkpoints/model-{model_file_name}-best.pth"
+                torch.save(net.state_dict(), path)
+                min_val_loss = val_loss
             path = f"checkpoints/model-{model_file_name}-{epoch}.pth"
             torch.save(net.state_dict(), path)
 
-            training_progression_animator.add(epoch + 1, (
-                metrics['train'][0] / metrics['train'][2],
-                metrics['train'][1] / metrics['train'][2],
-                metrics['val'][0] / metrics['val'][2],
-                metrics['val'][1] / metrics['val'][2]
-            ))
+            if graph:
+                training_progression_animator.add(epoch, (
+                    metrics['train'][0] / metrics['train'][2],
+                    metrics['train'][1] / metrics['train'][2],
+                    val_loss,
+                    metrics['val'][1] / metrics['val'][2]
+                ))
         else:
-            training_progression_animator.add(epoch + 1, (
-                metrics['train'][0] / metrics['train'][2],
-                metrics['train'][1] / metrics['train'][2]
-            ))
+            if graph:
+                training_progression_animator.add(epoch, (
+                    metrics['train'][0] / metrics['train'][2],
+                    metrics['train'][1] / metrics['train'][2]
+                ))
 
     test_loss, test_acc = evaluate(net, data_loaders['test'], loss_fn, device)
     val_loss, val_acc = evaluate(net, data_loaders['val'], loss_fn, device)
@@ -200,8 +211,69 @@ def train(net: nn.Module, data_loaders: Dict[str, DataLoader], optimizer: torch.
           f'val loss {val_loss:.3f}, val accuracy {val_acc:.3f}, '
           f'test loss {test_loss:.3f}, test accuracy {test_acc:.3f}')
 
+    return train_loss, train_acc, val_loss, val_acc, test_loss, test_acc
+
+
 # -----------------------------------------------------------------------------
 # Pruning
 # -----------------------------------------------------------------------------
 
+def prune_random(net: nn.Sequential, p: float):
+    for layer, last in is_last(net.children()):
+        if last:
+            if isinstance(layer, nn.Linear):
+                nn.utils.prune.random_unstructured(layer, 'weight', amount=p / 2)
+        if isinstance(layer, nn.Linear):
+            nn.utils.prune.random_unstructured(layer, 'weight', amount=p)
+
+
 # TODO: Put functions related to pruning here
+def copy_prune(mask_net: nn.Sequential, apply_net: nn.Sequential):
+    for mask_layer, apply_layer in zip(mask_net.children(), apply_net.children()):
+        if isinstance(mask_layer, nn.Linear):
+            weight_mask = mask_layer.state_dict().get('weight_mask')
+            nn.utils.prune.custom_from_mask(apply_layer, 'weight', weight_mask)
+
+
+def prune(net: nn.Sequential, p: float):
+    """
+    Prunes a network in-place, i.e., does not return a new network.
+
+    :param net: Network to prune
+    :param p: percentage of weights to pune
+    """
+    for layer, last in is_last(net.children()):
+        if last:
+            if isinstance(layer, nn.Linear):
+                nn.utils.prune.l1_unstructured(layer, 'weight', amount=p / 2)
+        if isinstance(layer, nn.Linear):
+            nn.utils.prune.l1_unstructured(layer, 'weight', amount=p)
+
+
+def is_last(i: Iterator):
+    """Pass through all values from the given iterable, augmented by the
+    information if there are more values to come after the current one
+    (True), or if it is the last value (False).
+    """
+    # Copied from https://stackoverflow.com/a/1630350/21752100
+    # Get an iterator and pull the first value.
+    last = next(i)
+    # Run the iterator to exhaustion (starting from the second value).
+    for val in i:
+        # Report the *previous* value (more to come).
+        yield last, True
+        last = val
+    # Report the last value.
+    yield last, False
+
+
+def _experiment_section_1(arch: str, dataset: str, optim, lr, fc_pruning_rate, conv_pruning_rate):
+    datasets = get_datasets(dataset, used_data=0.1)
+    net = create_network(arch)
+    device = try_gpu()
+
+    name = f'experiment1-{arch}-{dataset}'
+
+    train(net, datasets, optim, CrossEntropyLoss(), device, name, lr, 50, 5)
+
+
--- Assignment 7/simple_training.py
+++ Assignment 7/simple_training.py
@@ -15,7 +15,7 @@ def load(path: str):
     return model
 
 
-def train():
+def train(graph=True):
     arch_name = "lenet"
     dataset_name = "fashionmnist"
     run_name = "1"
@@ -24,7 +24,7 @@ def train():
     
     device_name = try_gpu()
     net = common.create_network(arch_name)
-    data_loaders = common.get_dataset(dataset_name, used_data=0.01)
+    data_loaders = common.get_datasets(dataset_name, used_data=0.01)
     
     common.train(
         net,
@@ -35,8 +35,10 @@ def train():
         epochs=10,
         device=device_name,
         model_file_name=model_name,
-        eval_every_n_epochs=2
+        eval_every_n_epochs=2,
+        graph=graph
     )
+    return net
 
 
 if __name__ == '__main__':
